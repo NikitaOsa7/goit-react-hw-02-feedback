2ecf8ae8d58958ad092b5797bbfdf95c3ddc84b6
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-06-03 15:49:59 +0300

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  state = {
+    good: 0,
+    netural: 0,
+    bad: 0,
+  }
 }
 
-export default App;
+onFeedbackLeave = evt => {
+  evt.preventDefault();
+  const element = evt.target.innerText.toLowerCase();
+  this.setState(state => ({
+    [element]: state[element] + 1,
+  }));
+};
--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,29 +1,27 @@
+import s from './FeedbackOptions.module.css';
 import PropTypes from 'prop-types';
-import s from './FeedbackOptions.module.css'
-
 
 export default function FeedbackOptions({ options, onFeedbackLeave }) {
-    const firstCapitalLetter = string =>
-        string[0].toUpperCase() + string.substring(1);
-    
-    return (
-        <>
-            {options.map((option, index) => (
-                <button
-                    key={index}
-                    type="button"
-                    className={s.button}
-                    onClick={onFeedbackLeave}
-                >
-                    {firstCapitalLetter(option)}
-                </button>
-            ))}
-        </>
-    );
-}
+  const firstLetterCaps = string =>
+    string[0].toUpperCase() + string.substring(1);
 
+  return (
+    <>
+      {options.map((option, index) => (
+        <button
+          key={index}
+          type="button"
+          className={s.button}
+          onClick={onFeedbackLeave}
+        >
+          {firstLetterCaps(option)}
+        </button>
+      ))}
+    </>
+  );
+}
 
 FeedbackOptions.propTypes = {
-    buttons: PropTypes.arrayOf(PropTypes.string.isRequired),
-    onFeedbackLeave: PropTypes.func.isRequired,
-}
+  buttons: PropTypes.arrayOf(PropTypes.string.isRequired),
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
